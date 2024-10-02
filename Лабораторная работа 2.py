0d541e408f5b3c139e78527ecb9a5cf9f94cef6d
prudkoyuv <prudkoyuv@yandex.ru>
2024-10-02 22:35:26 +0300

    лаба 2 конец

--- Лабораторная работа 2.py
+++ Лабораторная работа 2.py
@@ -1,5 +1,5 @@
 store = ['пятерка', 'лента', 'дикси']
-print(f'Введите товары через пробел')
+print('Введите товары через пробел')
 products = input().split(' ')
 price = [*range(len(products))]
 for i in range(len(products)):
@@ -7,4 +7,9 @@ for i in range(len(products)):
         f'Введите цены через пробел для {products[i]} в магазинах {" ".join(store)} соответственно')
     price[i] = input().split(' ')
 
-print(price)
+for i in range(len(store)):
+    s = 0
+    for j in range(len(price)):
+        s += int(price[j][i])
+    print(
+        f'цена в {store[i]} - {s}')
