75181c30ee3bb68a456a71402aa55c9cf214830a
prudkoyuv <prudkoyuv@yandex.ru>
2024-10-27 00:40:26 +0300

    рекурсия в лабе3

--- Лабораторная работа 3.py
+++ Лабораторная работа 3.py
@@ -1,3 +1,12 @@
-print(min_breaks(2, 3)) # Должно вывести 5
-print(min_breaks(3, 3)) # Должно вывести 8
-print(min_breaks(1, 1)) # Должно вывести 0 
+def min_breaks(n, m):
+    if n == 1 and m == 1:
+        return 0
+    if n != 1:
+        return min_breaks(n//2, m)+min_breaks(n-n//2, m)+1
+    else:
+        return min_breaks(n, m//2)+min_breaks(n, m-m//2)+1
+
+
+print(min_breaks(2, 3))  # Должно вывести 5
+print(min_breaks(3, 3))  # Должно вывести 8
+print(min_breaks(1, 1))  # Должно вывести 0
